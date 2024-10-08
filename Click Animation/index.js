a6e5f241940f3201400105ea6e5fd1699c8de7db
Jubayer Safin <109754142+safin4@users.noreply.github.com>
2024-10-08 20:08:40 +0600

    Add files via upload

---
+++ Click Animation/index.js
@@ -0,0 +1,40 @@
+loadConfettiPreset(tsParticles);
+let loadConfettiContainer;
+tsParticles
+.load("tsparticles", {
+    particles: {
+        shape: {
+            type: "character",
+            character: {
+                value: ["*"]
+            },
+            move: {
+                direction: "top"
+            },
+        },
+    },
+    preset: "confetti",
+    emitters: []
+})
+.then((container) => (confettiContainer = container));
+layout.addEventListener("click", () => {
+    confettiContainer.addEmitter({
+        size: {
+            width: 0,
+            height: 0
+        },
+        startCount: 340,
+        position: {
+            "x": 50,
+            "y": 50
+        },
+        rate: {
+            delay: 0,
+            quantity: 50
+        },
+        life: {
+            duration: 5,
+            count: 5
+        }
+    });
+});
